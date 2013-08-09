3ad10eb07fb472f16c3d4cb17dd02d266c1b62f7
Bunkie <bunkie.wang@gmail.com>
2013-08-08 21:17:38 -0700

    modified register.html and reformatted code

--- static/controllers/formCtrl.js
+++ static/controllers/formCtrl.js
@@ -2,14 +2,15 @@
 //	Form controller for error messages.
 //
 superTripperApp.controller('FormCtrl', function($scope) {
-    $scope.addMessages = function(messages) {
-        $scope.messages = $scope.messages || [];
-        $scope.messages = $scope.messages.concat(messages);
-    };
+  $scope.addMessages = function(messages) {
+    $scope.messages = $scope.messages || [];
+    $scope.messages = $scope.messages.concat(messages);
+  };
 
-    $scope.reset = function() {
-        $scope.messages = [];
-        $scope.form = {};
-    };
+  $scope.reset = function() {
+    $scope.messages = [];
+    $scope.form = {};
+  };
+
 });
 
